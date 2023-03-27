e0a05d22ca6e030e5258e987f5f2937d04337726
orlando1080 <lewislsewell@gmail.com>
2023-03-27 18:00:25 +0100

    Added new soloution

---
+++ 6_kyu/your_order_please.py
@@ -0,0 +1,9 @@
+# DESCRIPTION:
+# Your task is to sort a given string. Each word in the string will contain a single number. This number is the position
+# the word should have in the result.
+
+# My solution
+
+def order(sentence):
+    return ' '.join(word for index in range(len(sentence)) for word in sentence.split() if str(index + 1) in word)
+    
