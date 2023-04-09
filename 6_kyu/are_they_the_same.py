2d7238b75d37b1fe8820501bf84c1ea477ba6e52
orlando1080 <lewislsewell@gmail.com>
2023-04-09 15:06:08 +0100

    Added Solution

---
+++ 6_kyu/are_they_the_same.py
@@ -0,0 +1,11 @@
+# DESCRIPTION:
+# Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the
+# "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears).
+# "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+
+def comp(array1, array2):
+    try:
+        return sorted([number ** 2 for number in array1]) == sorted(array2)
+    except TypeError:
+        return False
