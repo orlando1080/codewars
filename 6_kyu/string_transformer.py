b62e094112f7efa58d89784c80b1fec51a0090ec
orlando1080 <lewislsewell@gmail.com>
2023-04-19 22:25:04 +0100

    Added solution

---
+++ 6_kyu/string_transformer.py
@@ -0,0 +1,10 @@
+# Description
+
+# Given a string, return a new string that has transformed based on the input:
+#
+# Change case of every character, ie. lower case to upper case, upper case to lower case.
+# Reverse the order of words from the input.
+# Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+def string_transformer(s):
+    return ' '.join(s.swapcase().split(' ')[::-1])
