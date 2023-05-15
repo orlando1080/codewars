2be0024f8ee1d692f566dafbaec3f4029771b44e
orlando1080 <lewislsewell@gmail.com>
2023-05-15 18:46:32 +0100

    Added solution

---
+++ 7_kyu/holiday_fire_on_the_boat.py
@@ -0,0 +1,14 @@
+# Enjoying your holiday, you head out on a scuba diving trip!
+
+# Disaster!! The boat has caught fire!!
+
+# You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring
+# into action. Change any instance of "Fire" into "~~". Then return the string.
+
+# Go to work!
+
+import re
+
+def fire_fight(s):
+    s = re.sub(r'Fire', '~~', s, re.IGNORECASE)
+    return s
