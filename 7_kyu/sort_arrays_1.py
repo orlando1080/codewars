3d18a8cee14a65e8bb4a2a5ffe9a4dfb29aef13a
orlando1080 <lewislsewell@gmail.com>
2023-05-22 21:52:06 +0100

    Added solution

---
+++ 7_kyu/sort_arrays_1.py
@@ -0,0 +1,7 @@
+# Description:
+
+# Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in
+# lexicographical order.
+
+def sortme(names):
+    return sorted(names)
